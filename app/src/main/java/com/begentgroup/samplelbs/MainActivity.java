e2d2f908289b26fbd2fa86b8327b25fabe418f4b
Yon Seungik <dongja94@gmail.com>
2016-08-17 17:17:34 +0900

    geocoder

--- app/src/main/java/com/begentgroup/samplelbs/MainActivity.java
+++ app/src/main/java/com/begentgroup/samplelbs/MainActivity.java
@@ -4,7 +4,9 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -15,8 +17,18 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,11 +38,19 @@ public class MainActivity extends AppCompatActivity {
 
     TextView messageView;
 
+    ListView listView;
+    ArrayAdapter<Address> mAdapter;
+    EditText keywordView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         messageView = (TextView) findViewById(R.id.text_message);
+        keywordView = (EditText)findViewById(R.id.edit_keyword);
+        listView = (ListView)findViewById(R.id.listView);
+        mAdapter = new ArrayAdapter<Address>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(mAdapter);
 
         mLM = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Criteria criteria = new Criteria();
@@ -46,8 +66,44 @@ public class MainActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestLocationPermission();
         }
+
+        Button btn = (Button)findViewById(R.id.btn_convert);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String keyword = keywordView.getText().toString();
+                if (!TextUtils.isEmpty(keyword)) {
+                    convertAddressToLocation(keyword);
+                }
+            }
+        });
     }
 
+    private void convertAddressToLocation(String keyword) {
+        if (Geocoder.isPresent()) {
+            Geocoder geocoder = new Geocoder(this, Locale.KOREAN);
+            try {
+                List<Address> list = geocoder.getFromLocationName(keyword, 10);
+                mAdapter.clear();
+                mAdapter.addAll(list);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void convertLocationToAddress(Location location) {
+        if (Geocoder.isPresent()) {
+            Geocoder geocoder = new Geocoder(this, Locale.KOREAN);
+            try {
+                List<Address> list = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 10);
+                mAdapter.clear();
+                mAdapter.addAll(list);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     private void requestLocationPermission() {
         if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
 
@@ -111,6 +167,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayLocation(Location location) {
         messageView.setText("lat : " + location.getLatitude() + ", lng : " + location.getLongitude());
+        convertLocationToAddress(location);
     }
 
     LocationListener mListener = new LocationListener() {
