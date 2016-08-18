df02f7a88b382c482ca6a12f30e0817c69e88bab
Yon Seungik <dongja94@gmail.com>
2016-08-18 13:21:28 +0900

    marker

--- app/src/main/java/com/begentgroup/samplelbs/TMapActivity.java
+++ app/src/main/java/com/begentgroup/samplelbs/TMapActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.PointF;
 import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.location.LocationListener;
@@ -12,8 +13,15 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import com.skp.Tmap.TMapMarkerItem;
+import com.skp.Tmap.TMapPOIItem;
+import com.skp.Tmap.TMapPoint;
 import com.skp.Tmap.TMapView;
+
+import java.util.ArrayList;
 
 public class TMapActivity extends AppCompatActivity {
 
@@ -46,7 +54,36 @@ public class TMapActivity extends AppCompatActivity {
         });
         mapView.setSKPMapApiKey("2bc7afe3-fc89-3125-b699-b9fb7cfe2fae");
         mapView.setLanguage(TMapView.LANGUAGE_KOREAN);
+
+        Button btn = (Button)findViewById(R.id.btn_add_marker);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TMapPoint point = mapView.getCenterPoint();
+                addMarker(point.getLatitude(), point.getLongitude(), "My Marker");
+            }
+        });
     }
+
+    private void addMarker(double lat, double lng, String title) {
+        TMapMarkerItem item = new TMapMarkerItem();
+        TMapPoint point = new TMapPoint(lat, lng);
+        item.setTMapPoint(point);
+        Bitmap icon = ((BitmapDrawable)ContextCompat.getDrawable(this, android.R.drawable.ic_input_add)).getBitmap();
+        item.setIcon(icon);
+        item.setPosition(0.5f, 1);
+        item.setCalloutTitle(title);
+        item.setCalloutSubTitle("sub " + title);
+        Bitmap left = ((BitmapDrawable)ContextCompat.getDrawable(this, android.R.drawable.ic_dialog_alert)).getBitmap();
+        item.setCalloutLeftImage(left);
+        Bitmap right = ((BitmapDrawable)ContextCompat.getDrawable(this, android.R.drawable.ic_input_get)).getBitmap();
+        item.setCalloutRightButtonImage(right);
+        item.setCanShowCallout(true);
+        mapView.addMarkerItem("m"+id,item);
+        id++;
+    }
+
+    int id = 0;
 
     boolean isInitialized = false;
 
@@ -61,6 +98,23 @@ public class TMapActivity extends AppCompatActivity {
             moveMap(cacheLocation.getLatitude(), cacheLocation.getLongitude());
             setMyLocation(cacheLocation.getLatitude(), cacheLocation.getLongitude());
         }
+        mapView.setOnClickListenerCallBack(new TMapView.OnClickListenerCallback() {
+            @Override
+            public boolean onPressEvent(ArrayList<TMapMarkerItem> arrayList, ArrayList<TMapPOIItem> arrayList1, TMapPoint tMapPoint, PointF pointF) {
+                return false;
+            }
+
+            @Override
+            public boolean onPressUpEvent(ArrayList<TMapMarkerItem> arrayList, ArrayList<TMapPOIItem> arrayList1, TMapPoint tMapPoint, PointF pointF) {
+                return false;
+            }
+        });
+        mapView.setOnCalloutRightButtonClickListener(new TMapView.OnCalloutRightButtonClickCallback() {
+            @Override
+            public void onCalloutRightButton(TMapMarkerItem tMapMarkerItem) {
+
+            }
+        });
     }
 
     @Override
